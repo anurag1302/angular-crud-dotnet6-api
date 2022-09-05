c7cf2abc5208a43460cf874763a37073fd4e3231
Anurag Sinha <anurag3487@gmail.com>
2022-09-06 00:32:56 +0530

    CRUD endpoints implemented

--- api/PersonsAPI/Controllers/PersonsController.cs
+++ api/PersonsAPI/Controllers/PersonsController.cs
@@ -38,7 +38,8 @@ namespace PersonsAPI.Controllers
         [Route("GetPersonById/{id}")]
         public async Task<BaseApiResponse<Person>> GetPersonById(Guid id)
         {
-            var person = await _context.Persons.FirstOrDefaultAsync(x => x.Id == id);
+            var person = await _context.Persons
+                .FirstOrDefaultAsync(x => x.Id == id);
 
             if (person == null)
             {
@@ -62,6 +63,107 @@ namespace PersonsAPI.Controllers
             };
 
             return response;
+        }
+
+        [HttpPost]
+        [Route("CreatePerson")]
+        public async Task<BaseApiResponse<Person>> CreatePerson([FromBody] Person person)
+        {
+            var model = await _context.Persons
+                .FirstOrDefaultAsync(x => x.Email == person.Email);
+
+            if (model != null)
+            {
+                return new BaseApiResponse<Person>()
+                {
+                    Data = null,
+                    IsSuccess = false,
+                    Errors = null,
+                    StatusCode = HttpStatusCode.BadRequest,
+                    Message = "Person already exists"
+                };
+            }
+            _context.Persons.Add(person);
+            await _context.SaveChangesAsync();
+            return new BaseApiResponse<Person>()
+            {
+                Data = person,
+                IsSuccess = true,
+                Errors = null,
+                StatusCode = HttpStatusCode.Created,
+                Message = "Created"
+            };
+        }
+
+        [HttpPut]
+        [Route("UpdatePerson")]
+        public async Task<BaseApiResponse<Person>> UpdatePerson([FromBody] Person person)
+        {
+            var model = await _context.Persons
+                .FirstOrDefaultAsync(x => x.Id == person.Id);
+
+            if (model == null)
+            {
+                return new BaseApiResponse<Person>()
+                {
+                    Data = null,
+                    IsSuccess = false,
+                    Errors = null,
+                    StatusCode = HttpStatusCode.NotFound,
+                    Message = "Person not exists"
+                };
+            }
+
+            model.Name = person.Name;
+            model.Phone = person.Phone;
+            model.Email = person.Email;
+            model.Address = person.Address;
+            model.PostalZip = person.PostalZip;
+            model.Country = person.Country;
+
+            _context.Persons.Update(model);
+            await _context.SaveChangesAsync();
+
+            return new BaseApiResponse<Person>()
+            {
+                Data = model,
+                IsSuccess = true,
+                Errors = null,
+                StatusCode = HttpStatusCode.OK,
+                Message = "Updated"
+            };
+        }
+
+        [HttpDelete]
+        [Route("DeletePerson")]
+        public async Task<BaseApiResponse<Person>> DeletePerson([FromBody] Guid id)
+        {
+            var model = await _context.Persons
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (model == null)
+            {
+                return new BaseApiResponse<Person>()
+                {
+                    Data = null,
+                    IsSuccess = false,
+                    Errors = null,
+                    StatusCode = HttpStatusCode.NotFound,
+                    Message = "Person not exists"
+                };
+            }
+
+            _context.Persons.Remove(model);
+            await _context.SaveChangesAsync();
+
+            return new BaseApiResponse<Person>()
+            {
+                Data = null,
+                IsSuccess = true,
+                Errors = null,
+                StatusCode = HttpStatusCode.OK,
+                Message = "Deleted"
+            };
         }
     }
 }
