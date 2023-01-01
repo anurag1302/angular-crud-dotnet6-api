5cd0717c8a3bd3c72219b110e5ce8e0ad35884bd
Anurag <anurag3487@gmail.com>
2023-01-01 23:56:10 +0530

    Update WeatherForecastController.cs

--- api/PersonsAPI/Controllers/WeatherForecastController.cs
+++ api/PersonsAPI/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace PersonsAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+         "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
